ddd8891168251757d1a17b816047b1790bb0b5da
cruemartin92 <crue.c.martin@wmich.edu>
2021-07-19 11:38:10 -0400

    added score system

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ FPS = 60
 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+GREEN = (0,255,0)
 
 
 class Ball():
@@ -15,9 +16,9 @@ class Ball():
         self.y = Y
         self.radius = radius
         self.screen = screen
-        self.vert_speed = 0
-        self.horz_speed = -10
-        self.rect = pygame.draw.circle(self.screen, WHITE, (self.x,self.y), self.radius)
+        self.vert_speed = 5
+        self.horz_speed = -5
+        self.rect = pygame.draw.circle(self.screen, GREEN, (self.x,self.y), self.radius)
 
         self.left =  self.x - self.radius
         self.right = self.x + self.radius
@@ -25,7 +26,7 @@ class Ball():
         self.bottom = self.y + self.radius
 
     def draw(self):
-        self.rect = pygame.draw.circle(self.screen, WHITE, (self.x,self.y), self.radius)
+        self.rect = pygame.draw.circle(self.screen, GREEN, (self.x,self.y), self.radius)
 
     def move(self):
         self.x += self.horz_speed
@@ -36,6 +37,14 @@ class Ball():
 
     def flip_vert_speed(self):
         self.vert_speed = -self.vert_speed
+
+    def collision(self, paddel):
+
+        ball_tlc_x = self.x - self.radius
+        ball_tlc_y = self.y - self.radius
+
+        return paddel.x <= ball_tlc_x and ball_tlc_x <= paddel.x + paddel.width and paddel.y <= ball_tlc_y and ball_tlc_y <= paddel.y+paddel.height
+           
 
 class paddel():
     
@@ -64,27 +73,9 @@ class paddel():
             self.y += self.vert_speed
 
 
-def collision(ball, paddel):
-
-    ball_tlc_x = ball.x - ball.radius
-    ball_tlc_y = ball.y - ball.radius
-
-
-    if paddel.x <= ball_tlc_x and ball_tlc_x <= paddel.x + paddel.width and paddel.y <= ball_tlc_y and ball_tlc_y <= paddel.y+paddel.height:
-        print("collison")
+def court(screen):
     
-
-    
-# def in_paddel(ball, paddel):
-
-#     ball_top_y = ball.y - ball.radius
-#     ball_top_x = ball.x - ball.radius
-
-#     paddel_bottom_x =  paddel.x + paddel.width
-#     paddel_bottom_y =  paddel.y + paddel.height
-
-#     if ball_top_y >= paddel.y and ball_top_y <= paddel_bottom_y and ball_top_x >= paddel.x and ball_top_x <= paddel_bottom_y:
-#         print("yep") 
+    pygame.draw.line(screen, WHITE, (screen.get_width()/2 -3 ,0), (screen.get_width()/2 - 3 , screen.get_height()) , width= 3 )
 
 
 
@@ -108,6 +99,9 @@ def main():
 
     player_left = paddel(0, 200, screen)
     player_right =  paddel(WIDTH-50, 200, screen)
+
+    player_left_score = 0
+    player_right_score = 0
 
 
 
@@ -137,10 +131,6 @@ def main():
 
         ball.move()
 
-        collision(ball, player_left)   
-
-        collision(ball, player_right)
-
         #check top
         if ball.y - ball.radius <= 0:
             ball.flip_vert_speed()
@@ -150,20 +140,34 @@ def main():
 
         #check left
         if ball.x - ball.radius <= 0:
+            player_right_score += 1
             ball.flip_horz_speed()
 
         #check right
         if ball.x + ball.radius >= WIDTH:
+            player_left_score += 1
             ball.flip_horz_speed()
 
         #check left paddel collison
-        
-    
+        if ball.collision(player_left):
+            ball.x = player_left.x + player_left.width + ball.radius
+            ball.flip_horz_speed()
+
         #check right paddel collision
-        
+        if ball.collision(player_right)  :
+            ball.x = player_right.x - ball.radius
+            ball.flip_horz_speed()
 
 
         screen.blit(background, (0,0))
+
+        font = pygame.font.Font(None, 50)
+        text = font.render(str(player_left_score), 1, WHITE)
+        screen.blit(text, (screen.get_width()/2 - (text.get_width() + 10) , screen.get_height()/2 - 25))
+        text = font.render(str(player_right_score), 1, WHITE)
+        screen.blit(text, (screen.get_width()/2 + 5, screen.get_height()/2 - 25))
+
+        court(screen)
 
         ball.draw()
         player_left.draw()
