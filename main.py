38a24a754fad0fbf0109359a3efa378d6809a677
cruemartin92 <crue.c.martin@wmich.edu>
2021-07-19 16:13:30 -0400

    added start and pause screen

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class Ball():
         return paddel.x <= ball_tlc_x and ball_tlc_x <= paddel.x + paddel.width and paddel.y <= ball_tlc_y and ball_tlc_y <= paddel.y+paddel.height
            
 
-class paddel():
+class Paddel():
     
     def __init__(self, X,Y,screen):
         self.x = X
@@ -80,7 +80,7 @@ def court(screen):
 
 
 def main():
-    pygame.init()
+    # pygame.init()
 
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -97,8 +97,8 @@ def main():
 
     ball = Ball(305,305, 10, screen)
 
-    player_left = paddel(0, 200, screen)
-    player_right =  paddel(WIDTH-50, 200, screen)
+    player_left = Paddel(0, 200, screen)
+    player_right =  Paddel(WIDTH-50, 200, screen)
 
     player_left_score = 0
     player_right_score = 0
@@ -110,21 +110,25 @@ def main():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                quit()
         
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                return
+                quit()
 
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+                pause(screen)
+
+        #check keys pressed for paddel movement
         keys = pygame.key.get_pressed()
 
+        #right paddel
         if keys[pygame.K_UP]:
             player_right.move_up()
         if keys[pygame.K_DOWN]:
             player_right.move_down()
-
+        #left paddel
         if keys[pygame.K_w]:
             player_left.move_up()
-
         if keys[pygame.K_s]:
             player_left.move_down()
 
@@ -161,19 +165,94 @@ def main():
 
         screen.blit(background, (0,0))
 
+
+        #display scrore
         font = pygame.font.Font(None, 50)
         text = font.render(str(player_left_score), 1, WHITE)
         screen.blit(text, (screen.get_width()/2 - (text.get_width() + 10) , screen.get_height()/2 - 25))
         text = font.render(str(player_right_score), 1, WHITE)
         screen.blit(text, (screen.get_width()/2 + 5, screen.get_height()/2 - 25))
 
+        
+        #draw
         court(screen)
-
         ball.draw()
         player_left.draw()
         player_right.draw()
 
         pygame.display.flip()
 
+def title_screen():
+    pygame.init()
+
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    pygame.display.set_caption("Pong!")
+
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill(BLACK)
+
+    screen.blit(background, (0,0))
+    pygame.display.flip()
+
+    clock = pygame.time.Clock()
+
+
+    while True:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+        
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                quit()
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_2:
+                main() 
+
+        title_font = pygame.font.Font(None, 100)
+        title = title_font.render("PONG!", 1, WHITE)
+        screen.blit(title, ((screen.get_width()/2) - title.get_width()/2, screen.get_height()/3))
+        instruction_font = pygame.font.Font(None, 50)
+        instruction = instruction_font.render("Press [2] to start a 2 player game!", 1, WHITE)
+        screen.blit(instruction, (screen.get_width()/2 - instruction.get_width()/2, screen.get_height()/2))
+        signature_font = pygame.font.Font(None, 25)
+        signature = signature_font.render("Made by Crue Martin using pygame!", 1, WHITE)
+        screen.blit(signature, (screen.get_width()/2 - signature.get_width()/2, screen.get_height() - (signature.get_height() + 10)))
+
+        pygame.display.flip()
+
+def pause(screen):
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                quit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_c:
+                return
+        
+        # background = pygame.Surface(screen.get_size())
+        # background = background.convert()
+        # background.fill(BLACK)
+
+        screen.fill(BLACK)
+
+
+        paused_font = pygame.font.Font(None, 80)
+        paused = paused_font.render("Game Paused", 1, WHITE)
+        screen.blit(paused, (screen.get_width()/2 - paused.get_width()/2, screen.get_height()/3))
+        instruction_font = pygame.font.Font(None, 50)
+        instruction = instruction_font.render("Press [c] to continue...",1, WHITE)
+        screen.blit(instruction, (screen.get_width()/2 - instruction.get_width()/2, screen.get_height()/2))
+
+        # screen.blit(background, (0,0))
+        pygame.display.flip()
+
 if __name__ == "__main__":
-    main()
+    title_screen()
+    # main()
